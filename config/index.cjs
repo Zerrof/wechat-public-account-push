4159dfbb022ae000299149f1d8d3596c682fd770
zhaoyufei <18332821053@163.com>
2024-02-23 14:46:16 +0800

    fix：修改

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'ov2pM62Yp038k_8Y-yiC8JVP-938',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'eES0FtNPZwqvEqns1u4C_6yhfsNASek0AR8sIdPTCnI',
+      useTemplateId: 'gzLBocWuU0ka__Qj2uRaUzYd2NmWTvuHRaOxmgqxMU4',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '',
       festivals: [
@@ -51,7 +51,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'he4WTvyqte6OoR8hhu1xGebCSqRkCl606TpIhoZvMVg',
+  CALLBACK_TEMPLATE_ID: 'YepCj7-uv1csaHbhAwLrXcBsELOAX7_PspDl8HGf0-s',
 
   CALLBACK_USERS: [
     {
--- config/template-config.cjs
+++ config/template-config.cjs
@@ -1,43 +1,61 @@
 /* eslint-disable */
 const TEMPLATE_CONFIG = [
   {
-    id: '0001',
-    title: '亲爱的, 早上好',
-    desc: `
-      **{{date.DATA}}**
+    id: "0001",
+    title: "亲爱的老婆！",
+    desc:
+      // `
+      //   **{{date.DATA}}**
+
+      //   下个休息日：{{holidaytts.DATA}}
+
+      //   ---
+
+      //   城市：{{city.DATA}}
+
+      //   天气：{{weather.DATA}}
+
+      //   气温(最高/最低):{{max_temperature.DATA}} / {{min_temperature.DATA}}
+
+      //   风向: {{wind_direction.DATA}}
+
+      //   风级: {{wind_scale.DATA}}
+
+      //   {{comprehensive_horoscope.DATA}}
+
+      //   ---
+
+      //   今天是我们相识的第{{love_day.DATA}}天
+
+      //   {{birthday_message.DATA}}
+
+      //   ---
+
+      //   {{moment_copyrighting.DATA}}
+
+      //   {{poetry_title.DATA}} {{poetry_content.DATA}}
+      // `
+      `🗓️{{date.DATA}}
+
+      💕今天是我们在一起的第{{love_day.DATA}}天，爱你❤️
       
-      下个休息日：{{holidaytts.DATA}}
-      
-      ---
-      
-      城市：{{city.DATA}}
-      
-      天气：{{weather.DATA}}
-      
-      气温(最高/最低):{{max_temperature.DATA}} / {{min_temperature.DATA}}
-      
-      风向: {{wind_direction.DATA}}
-      
-      风级: {{wind_scale.DATA}}
-      
-      {{comprehensive_horoscope.DATA}}
-      
-      ---
-      
-      今天是我们相识的第{{love_day.DATA}}天
-      
-      {{birthday_message.DATA}}
-      
-      ---
-      
-      {{moment_copyrighting.DATA}}
-      
-      {{poetry_title.DATA}} {{poetry_content.DATA}}
-    `
+      今日{{city.DATA}}天气☁️：{{tian_api_weather_weather_0.DATA}}
+
+      温度🌡️: {{tian_api_weather_lowest_0.DATA}}-{{tian_api_weather_highest_0.DATA}}
+
+      紫外线强度指数☀️: {{tian_api_weather_uv_index_0.DATA}}
+
+      穿衣指数👚: {{tian_api_weather_tips_0.DATA}}
+
+      风向🍃: {{wind_direction.DATA}}
+
+      风级💨: {{wind_scale.DATA}}
+
+      💌{{earthy_love_words.DATA}}`,
   },
   {
-    id: '0002',
-    title: '推送完成提醒',
+    id: "0002",
+    title: "推送完成提醒",
     desc: `
       服务器信息：{{post_time_zone.DATA}} {{post_time.DATA}}
       
@@ -50,8 +68,8 @@ const TEMPLATE_CONFIG = [
       成功用户: {{success_post_ids.DATA}}
       
       失败用户: {{fail_post_ids.DATA}}
-    `
+    `,
   },
-]
+];
 
-module.exports = TEMPLATE_CONFIG
+module.exports = TEMPLATE_CONFIG;
